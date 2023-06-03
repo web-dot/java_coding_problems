f13b9e0170e4c8228a68d2b41c42d10c277c677c
sudipto <cg.sudipc@gmail.com>
2023-06-03 12:31:07 +0530

    string problems

--- src/com/problems/StringNumbersMath/StringProblems.java
+++ src/com/problems/StringNumbersMath/StringProblems.java
@@ -1,11 +1,17 @@
 package com.problems.StringNumbersMath;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class StringProblems {
 	
@@ -94,14 +100,117 @@ public class StringProblems {
 		}
 		return Character.MIN_VALUE;
 	}
-		
+	
+	
+	/**
+	 * 3. Reversing a string
+	 * */
+	private static final String WHITESPACE = " ";
+	public static String reverseWord(String str) {
+		String[] words = str.split(WHITESPACE);
+		StringBuilder reverseString = new StringBuilder();
+		for(String word: words) {
+			StringBuilder reverseWord = new StringBuilder();
+			for(int i=word.length()-1; i>=0; i--) {
+				reverseWord.append(word.charAt(i));
+			}
+			reverseString.append(reverseWord).append(WHITESPACE);
+		}
+		return reverseString.toString();
+	}
+	
+	
+	// obtaining same result in functional style
+	
+	/**
+	 * uses regular expressions and stream to split the input string into 
+	 * substrings, then reverse each substring using StringBuilder, and
+	 * then joining the reversed substrings back together with spaces in between
+	 * */
+	private static final Pattern PATTERN = Pattern.compile(" +");
+	private static String reverseStringInFunctional(String str) {
+		return PATTERN.splitAsStream(str)
+				.map(w -> new StringBuilder(w).reverse())
+				.collect(Collectors.joining(" "));
+	}
+	
+	/**
+	 * 4. checking whether a string contains only digits
+	 * 
+	 * */
+	public static boolean containsOnlyDigits(String str) {
+		for(int i=0; i<str.length(); i++) {
+			if(!Character.isDigit(str.charAt(i))) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	
+	// using streams
+	public static boolean stringIsDigits(String str) {
+		return str.chars().anyMatch(n -> !Character.isDigit(n));
+	}
+	
+	// using stream.matches()
+	public static boolean stringIsDigitsMatches(String str) {
+		return str.matches("[0-9]+");
+	}
+	
+	
+	/**
+	 * 5. Counting vowels and consonants
+	 * */
+	private static final Set<Character> allVowels = new HashSet(Arrays.asList('a','e','i','o','u'));
+	public static List<Integer> countVowelsAndConsonants(String str){
+		List<Integer> list = new ArrayList<>();
+		str = str.toLowerCase();
+		int vowels = 0;
+		int consonants = 0;
+		for(int i=0; i<str.length(); i++) {
+			char ch = str.charAt(i);
+			if(allVowels.contains(ch)) {
+				vowels++;
+			}
+			else if(ch >= 'a' && ch <= 'z') {
+				consonants++;
+			}
+		}
+		list.add(vowels);
+		list.add(consonants);
+		return list;
+	}
+	
+	// using streams
+	public static List<Long> countVandC(String str){
+		List<Long> list = new ArrayList<>();
+		str = str.toLowerCase();
+		long vowels = str.chars()
+		.filter(c -> allVowels.contains((char)c))
+		.count();
+		long consonants = str.chars()
+				.filter(c -> !allVowels.contains((char)c))
+				.count();
+		list.add(vowels);
+		list.add(consonants);
+		return list;
+	}
 			
 	public static void main(String[] args) {
-		System.out.println(findFirstNonRepeatingChar("assistance"));
+		System.out.println(countVandC("hello"));
 		
 		/**
 		 * Notes
 		 * */
+		
+		// problem 3
+		Pattern p = Pattern.compile("-");
+		String schedule = "MUMBAI-17:50";
+		Stream<String> str = p.splitAsStream(schedule);
+		str.forEach(System.out::println);
+		
+		
 		
 		// problem 1
 		Map<Character, Integer> map = new HashMap<>();
