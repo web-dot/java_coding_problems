4a6c1917aa05d42ee8d163ec90455e3f0bb93104
sudipto <cg.sudipc@gmail.com>
2023-06-16 23:59:20 +0530

    starting null test

---
+++ src/com/problems/objects_immutability/NullReferenceTest.java
@@ -0,0 +1,18 @@
+package com.problems.objects_immutability;
+
+import java.util.List;
+
+public class NullReferenceTest {
+
+	
+	/**
+	 * 1. Checking null reference in functional style and imperative code
+	 * */
+	public static List<Integer> evenIntegers(List<Integer> integers){
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
